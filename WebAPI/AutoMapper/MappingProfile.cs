6993af30aec3258fcd8897d0a3a173f6c7d94faa
Neven Klofutar <neven.klofutar@gmail.com>
2021-02-27 21:35:54 +0100

    Added AutoMapper

---
+++ WebAPI/AutoMapper/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Entities;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.AutoMapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserDTO>();
+        }
+    }
+}
