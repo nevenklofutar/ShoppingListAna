2c56b1bf302d8cb3d2c3c160af3350d4c554a9d4
Neven Klofutar <neven.klofutar@gmail.com>
2021-02-27 21:41:12 +0100

    Added GroupDTO and ItemDTO

---
+++ Entities/DTOs/GroupDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DTOs
+{
+    public class GroupDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
